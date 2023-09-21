a6dfbadf4e3b8252e867c5cd372eebfd6251cf56
guifroes1984 <guilhermefroes26@gmail.com>
2023-09-20 23:25:41 -0300

    Passando parâmetros para o serviço

--- src/main/java/br/com/guifroes1984/controller/IndexController.java
+++ src/main/java/br/com/guifroes1984/controller/IndexController.java
@@ -4,15 +4,20 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController /*Arquitetura REST*/
 @RequestMapping(value = "/usuario")
 public class IndexController {
 	
+	/*Serviço RESTfull*/
 	@GetMapping(value = "/", produces = "application/json")
-	public ResponseEntity init() {
-		return new ResponseEntity("Olá Mundo", HttpStatus.OK);
+	public ResponseEntity init(@RequestParam (value = "nome", required = true, defaultValue = "Nome não informado") String nome, @RequestParam("salario") Long salario) {
+		
+		System.out.println("Parametro sendo recebido " + nome);
+		
+		return new ResponseEntity("Olá usuário REST Spring Boot seu nome é: " + nome + " salario é: " + salario, HttpStatus.OK);
 	}
 
 }
