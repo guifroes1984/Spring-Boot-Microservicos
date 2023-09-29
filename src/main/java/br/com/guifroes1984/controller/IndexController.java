7ce75170ffdc946d27a01c4176bfdf56eafe93be
guifroes1984 <guilhermefroes26@gmail.com>
2023-09-28 21:45:07 -0300

    Implementando o END-POINT de atualizacao

--- src/main/java/br/com/guifroes1984/controller/IndexController.java
+++ src/main/java/br/com/guifroes1984/controller/IndexController.java
@@ -83,6 +83,10 @@ public class IndexController {
 	@PutMapping(value = "/", produces = "application/json")
 	public ResponseEntity<Usuario> update(@RequestBody Usuario usuario) {
 		
+		for (int pos = 0; pos < usuario.getTelefones().size(); pos++) {
+			usuario.getTelefones().get(pos).setUsuario(usuario);
+		}
+		
 		Usuario usuarioSalvo = usuarioRepository.save(usuario);
 		
 		return new ResponseEntity<Usuario>(usuarioSalvo, HttpStatus.OK);
