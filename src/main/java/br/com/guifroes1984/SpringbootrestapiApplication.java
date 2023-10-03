10d01e86ae6d66ce1f60179bd75b995251c89558
guifroes1984 <guilhermefroes26@gmail.com>
2023-10-03 19:15:55 -0300

    Cross Origin - Configuracao centralizada

--- src/main/java/br/com/guifroes1984/SpringbootrestapiApplication.java
+++ src/main/java/br/com/guifroes1984/SpringbootrestapiApplication.java
@@ -8,7 +8,9 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EntityScan(basePackages = {"br.com.guifroes1984.model"})
@@ -18,10 +20,20 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @EnableWebMvc
 @RestController
 @EnableAutoConfiguration
-public class SpringbootrestapiApplication {
+public class SpringbootrestapiApplication implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootrestapiApplication.class, args);
+	}
+	
+	/*Mapeamento Global que refletem em todo o sistema*/
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		
+		registry.addMapping("/usuario/**")
+		.allowedMethods("*")
+		.allowedOrigins("*");
+		/*Liberando o mapeamento de usurio para todas as origens*/
 	}
 
 }
